013c06bf9ac08aa3bac2bd8d0e2c2cb36f494976
Yasir ajmal <Yasir ajmal@DESKTOP-2CNU89V>
2023-09-17 13:45:24 +0530

    First Commit

---
+++ src/main/java/com/yasirit/runner/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.yasirit.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("welcome to message runner.......");
+
+	}
+
+}

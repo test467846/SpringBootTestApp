67419993410ccc927dc37b7e8df70c2443cb0ff7
Yasir ajmal <Yasir ajmal@DESKTOP-2CNU89V>
2023-09-17 18:40:05 +0530

    Fourth commit

--- src/main/java/com/yasirit/runner/MessageRunner.java
+++ src/main/java/com/yasirit/runner/MessageRunner.java
@@ -1,15 +1,29 @@
 package com.yasirit.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
+
 @Component
 public class MessageRunner implements CommandLineRunner {
+	
+	private static Logger log= LoggerFactory.getLogger(MessageRunner.class);
 
 	@Override
 	public void run(String... args) throws Exception {
 		int a=10;
-		System.out.println("Hello......"+a);
+		try {
+			log.info("STARTED");
+			System.out.println("Hello......"+a);
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		log.info("END");
+		
 
 	}
 
